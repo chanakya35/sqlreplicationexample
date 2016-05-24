fe96c1dde07ef91e7b1ed54a663412f4a6204025
ramblinwreck35 <japatel@incomm.com>
2016-05-23 23:53:06 -0500

    Working Sql Replication example! :)

---
+++ CreateSqlTable.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SqlReplicationExample
+{
+    using System.Configuration;
+    using System.Data.SqlClient;
+
+    public class CreateSqlTable
+    {
+        public static void Run()
+        {
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RobotMinions"].ConnectionString))
+            {
+                conn.Open();
+                var isAlreadyRun = false;
+                using (var cmd = new SqlCommand("SELECT TOP 1 1 FROM sys.objects WHERE Name = 'Minions'", conn))
+                {
+                    isAlreadyRun = (int)(cmd.ExecuteScalar() ?? 0) == 1;
+                }
+                if (!isAlreadyRun)
+                {
+                    using (var cmd = new SqlCommand(@"
+                            CREATE TABLE Minions 
+                            ( 
+                                Id VARCHAR(50), 
+                                Name VARCHAR(200), 
+                                Age SMALLINT, RegisterDateTime DATETIME2, 
+                                CONSTRAINT PK_Minions PRIMARY KEY (Id)
+                            )", conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client.Document;
+using Raven.Database.Bundles.SqlReplication;
+
+namespace SqlReplicationExample
+{
+    class Program
+    {
+        /// <summary>
+        /// This sample requires the following prerequisites:
+        /// (a) A RavenDB instance running locally on port 8080, containing a database called "MyLittleDatabase" with SQL Replication bundle enabled
+        /// (b) A SQL Server instance running on localhost with a database called "RobotMinions"
+        /// (c) A SQL login with write access to the "RobotMinions" database which matches the identity under which the RavenDB instance is running
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            CreateSqlTable.Run();
+            InsertSampleRavenData();
+            CreateAndRunReplicationProfile();
+        }
+
+        private static void CreateAndRunReplicationProfile()
+        {
+            using (var session = (new DocumentStore() {ConnectionStringName = "RavenDb"}.Initialize().OpenSession()))
+            {
+                session.Store(new SqlReplicationConfig
+                {
+                    Id = "Raven/SqlReplication/Configuration/Minions",
+                    Name = "Minions",
+                    ConnectionString = ConfigurationManager.ConnectionStrings["RobotMinions"].ConnectionString,
+                    FactoryName = "System.Data.SqlClient",
+                    RavenEntityName = "Minions",
+                    SqlReplicationTables =
+                    {
+                        new SqlReplicationTable
+                        {
+                            TableName = "Minions",
+                            DocumentKeyColumn = "Id"
+                        }
+                    },
+                    Script = @"
+                        var minionData = {
+                            Id: documentId,
+                            Name: this.Name,
+                            Age: this.Age,
+                            RegisterDateTime: this.RegisterDateTime
+                        };
+
+                        replicateToMinions(minionData);                        
+                    "
+                });
+                session.SaveChanges();
+            }
+        }
+
+        static void InsertSampleRavenData(int numberOfMinions = 10)
+        {
+            using (var session = (new DocumentStore() {ConnectionStringName = "RavenDb"}.Initialize().OpenSession()))
+            {
+                for (int i = 0; i < numberOfMinions; i++)
+                {
+                    session.Store(new Minion
+                    {
+                        Name = "Robie " + new Random().Next(),
+                        Age = new Random().Next(23, 35),
+                        RegisterDateTime = DateTime.UtcNow
+                    });
+                }
+                session.SaveChanges();
+            }
+        }
+    }
+}
